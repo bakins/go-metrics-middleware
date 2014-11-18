f964c5edf2d88e21b3ed4e128f9968ab1974a7e3
Brian Akins <bakins@omniti.com>
2014-11-18 18:26:21 -0500

    add example

--- mmw_test.go
+++ mmw_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	metrics "github.com/armon/go-metrics"
 	"github.com/bakins/go-metrics-map"
@@ -72,4 +73,40 @@ func TestHandlerWrapper(t *testing.T) {
 	v, ok := s.Get("test.testing.count")
 	h.Assert(t, ok, "key not found")
 	h.Assert(t, v == 1, "value is not 1")
+}
+
+func ExampleHandler() {
+	// create a sink to use
+	inm := metrics.NewInmemSink(10*time.Millisecond, 50*time.Millisecond)
+
+	// create a default metrics config, without hostname
+	conf := metrics.DefaultConfig("test")
+	conf.EnableHostname = false
+
+	// now use that config and sink with metrics
+	m, _ := metrics.New(conf, inm)
+
+	// simple http handler
+	handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(200)
+		fmt.Fprint(w, "Hello World\n")
+	})
+
+	// create a new mw wrapping the metrics
+	mw := New(m)
+
+	// wrap the handler and add to a route
+	http.Handle("/foo", mw.Handler(handler, "testing"))
+
+	// every time "/foo" is access, the middleware will emit two metrics
+	// it will increment the counter "test.testing.count"
+	// and add a time sample to "test.testing.time"
+
+	http.Handle("/bar", mw.Handler(handler, "another"))
+
+	// every time "/bar" is access, the middleware will emit two metrics
+	// it will increment the counter "test.another.count"
+	// and add a time sample to "test.another.time"
+
+	http.ListenAndServe(":8080", nil)
 }
